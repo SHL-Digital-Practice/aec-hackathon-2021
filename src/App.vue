7627f6cdddd02943e72f66489def3c0a17d6af72
Victor <61347148+vwnd@users.noreply.github.com>
2021-10-30 12:26:02 +0200

    divs

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-let counter = ref(0)
+let counter = ref(0);
 
 setInterval(() => {
-  counter.value++
-}, 1000)
+  counter.value++;
+}, 1000);
 </script>
 
 <template>
   <div>
     <header class="bg-white shadow" v-if="$route.meta.title">
       <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <h1
-          @click="counter = 0"
-          class="text-3xl font-bold leading-tight text-gray-900"
-        >
-          {{ $route.meta.title }} / {{ counter }}
+        <h1 @click="counter = 0" class="text-3xl font-bold leading-tight text-gray-900">
+          {{ $route.meta.title }}
         </h1>
       </div>
     </header>
